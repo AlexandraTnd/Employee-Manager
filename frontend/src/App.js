119e100cc3e609fc11f0f510158d70dc9b7b8ba2
Elena Alexandra Trandafir <atrandafir7@gmail.com>
2024-06-30 15:07:21 +0300

    add browserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from './routes/Main';
+
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1><span className="text-primary">Employee</span> Manager</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Main />} />
+          <Route path="blogs" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
