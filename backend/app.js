523a9c825a0a9bc86963ab43b5da70f561a282d5
Elena Alexandra Trandafir <atrandafir7@gmail.com>
2024-06-30 14:18:37 +0300

    send employees data route from json

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 4000;
+const fs = require('fs')
 
 app.use((req, res, next) => {
     console.log(req.path);
     next();
 });
 
-app.use(express.static('public/img'));
+app.use(express.static('assets/img'));
+app.set('json spaces', 2);
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    const employeesData = JSON.parse(fs.readFileSync('./assets/data/employees.json'));
+    res.json(employeesData);
 })
 
 app.all('/*', (req, res) => {
     switch (req.path) {
         default:
             res.status(404);
-            res.sendFile(path.join(__dirname, '/public/error.html'));
+            res.sendFile(path.join(__dirname, '/assets/error.html'));
     }
 })
 
