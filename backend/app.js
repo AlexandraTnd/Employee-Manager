3be639edb6065bbdcb289db7f18c0a6618b751e6
Elena Alexandra Trandafir <atrandafir7@gmail.com>
2024-06-30 13:48:38 +0300

    add server error response to invalid requests

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 4000;
 
 app.use((req, res, next) => {
     console.log(req.path);
     next();
-  });
+});
+
+app.use(express.static('public/img'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World');
 })
 
-app.listen(4000)
+app.all('/*', (req, res) => {
+    switch (req.path) {
+        default:
+            res.status(404);
+            res.sendFile(path.join(__dirname, '/public/error.html'));
+    }
+})
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log(`Server succesfully running: http://localhost:${port}/ `)
+    } else {
+        console.log("Error occurred, server can't start", error);
+    }
+})
