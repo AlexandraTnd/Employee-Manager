45bc81dcc937fe7776473ec9d1ee8aeeb5ce10fe
Elena Alexandra Trandafir <atrandafir7@gmail.com>
2024-06-30 20:36:58 +0300

    add buttons to toggle sort by name position and hire date

--- frontend/src/components/EmployeeTable.js
+++ frontend/src/components/EmployeeTable.js
@@ -2,6 +2,78 @@ import { useState, useEffect } from 'react';
 
 function EmployeeTable() {
     const [employees, setEmployees] = useState();
+    const [isAscendingByName, setIsAscendingByName] = useState(false);
+    const [isAscendingByPosition, setIsAscendingByPosition] = useState(false);
+    const [isAscendingByHireDate, setIsAscendingByHireDate] = useState(false);
+
+
+    function sortByName() {
+        if (!isAscendingByName) {
+            const orderedEmployees = [...employees].sort((a, b) => {
+                if (a.lastName.toLowerCase() < b.lastName.toLowerCase()) {
+                    return -1;
+                }
+                if (a.lastName.toLowerCase() > b.lastName.toLowerCase()) {
+                    return 1;
+                }
+                return 0;
+            });
+            setIsAscendingByName(true);
+            setEmployees(orderedEmployees);
+        } else {
+            const orderedEmployees = [...employees].sort((a, b) => {
+                if (a.lastName.toLowerCase() < b.lastName.toLowerCase()) {
+                    return 1;
+                }
+                if (a.lastName.toLowerCase() > b.lastName.toLowerCase()) {
+                    return -1;
+                }
+                return 0;
+            });
+            setIsAscendingByName(false);
+            setEmployees(orderedEmployees);
+        }
+    }
+
+    function sortByPosition() {
+        if (!isAscendingByPosition) {
+            const orderedEmployees = [...employees].sort((a, b) => {
+                if (a.position.toLowerCase() < b.position.toLowerCase()) {
+                    return -1;
+                }
+                if (a.position.toLowerCase() > b.position.toLowerCase()) {
+                    return 1;
+                }
+                return 0;
+            });
+            setIsAscendingByPosition(true);
+            setEmployees(orderedEmployees);
+        } else {
+            const orderedEmployees = [...employees].sort((a, b) => {
+                if (a.position.toLowerCase() < b.position.toLowerCase()) {
+                    return 1;
+                }
+                if (a.position.toLowerCase() > b.position.toLowerCase()) {
+                    return -1;
+                }
+                return 0;
+            });
+            setIsAscendingByPosition(false);
+            setEmployees(orderedEmployees);
+        }  
+    }
+
+    function sortByHireDate() {
+        if (!isAscendingByHireDate) {
+            const orderedEmployees = [...employees].sort((a, b) => new Date(a.hireDate) - new Date(b.hireDate));
+            setIsAscendingByHireDate(true);
+            setEmployees(orderedEmployees);
+        } else {
+            const orderedEmployees = [...employees].sort((a, b) => new Date(b.hireDate) - new Date(a.hireDate));
+            setIsAscendingByHireDate(false);
+            setEmployees(orderedEmployees);
+        }  
+    }
 
     useEffect(() => {
         async function getData() {
@@ -17,27 +89,28 @@ function EmployeeTable() {
         <table>
             <thead>
                 <tr>
-                    <th style={{width: "5%"}}></th>
-                    <th style={{width: "20%"}}>Name</th>
-                    <th style={{width: "40%"}}>Address</th>
-                    <th style={{width: "10%"}}>Position</th>
-                    <th style={{width: "10%"}}>Hire Date</th>
+                    <th style={{ width: "5%" }}></th>
+                    <th style={{ width: "20%" }}>Name<button onClick={() => sortByName()}><i className="fa-solid fa-sort"></i></button></th>
+                    <th style={{ width: "40%" }}>Address</th>
+                    <th style={{ width: "10%" }}>Position<button onClick={() => sortByPosition()}><i className="fa-solid fa-sort"></i></button></th>
+                    <th style={{ width: "10%" }}>Hire Date<button onClick={() => sortByHireDate()}><i className="fa-solid fa-sort"></i></button></th>
                 </tr>
 
             </thead>
             <tbody>
-                { employees ? 
-                employees.map((employee, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{employee.firstName + " " + employee.lastName}</td>
-                            <td>{employee.address.street + " no. " + employee.address.streetNumber + ", " + employee.address.city}</td>
-                            <td className='text-center'>{employee.position}</td>
-                            <td className='text-center'>{employee.hireDate}</td>
-                        </tr>
-                    )}) : <tr><td colSpan={5} className='text-center'>No Data to view</td></tr>
-            }
+                {employees ?
+                    employees.map((employee, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{employee.lastName + " " + employee.firstName}</td>
+                                <td>{employee.address.street + " no. " + employee.address.streetNumber + ", " + employee.address.city}</td>
+                                <td className='text-center'>{employee.position}</td>
+                                <td className='text-center'>{employee.hireDate}</td>
+                            </tr>
+                        )
+                    }) : <tr><td colSpan={5} className='text-center'>No Data to view</td></tr>
+                }
             </tbody>
         </table>
     )
